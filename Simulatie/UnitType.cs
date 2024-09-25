0290174826b1645a14c3ba7a5923f743aad5a0b4
jesse <133413@hondsrugcollege.nl>
2024-09-25 10:08:33 +0200

    (unsaved work) WIP

---
+++ Simulatie/UnitType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulatie
+{
+    public interface IUnitType
+    {
+        int Id { get; set; }
+    }
+
+    public class UnitTypeProvider
+    {
+        public IUnitType GetUnit(SimulatedUnit unit, SimulationDatabaseContext db)
+        {
+            switch (unit.Type)
+            {
+                default:
+                    throw new Exception("Unknown unit type");
+            }
+        }
+    }
+}
