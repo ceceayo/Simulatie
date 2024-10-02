4f22d2e3b81260eb4f5b8f518ea7c4de30d04c40
jesse <133413@hondsrugcollege.nl>
2024-10-02 10:14:38 +0200

    add file logging, add create_simulation function.

--- Simulatie/Program.cs
+++ Simulatie/Program.cs
@@ -3,19 +3,32 @@ using System.Linq;
 using Simulatie;
 using Serilog;
 
+var folder = Environment.SpecialFolder.LocalApplicationData;
+var path = Environment.GetFolderPath(folder);
+var LogPath = System.IO.Path.Join(path, "simulatie.log");
+
 using var log = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console()
     .WriteTo.Debug()
+    .WriteTo.File(LogPath)
     .CreateLogger();
 Log.Logger = log;
 
 using var db = new SimulationDatabaseContext();
+Log.Information("Created a database {db}", db);
 
 // Note: This sample requires the database to be created before running.
 Log.Information("Database path is {DbPath}.", db.DbPath);
 
 var UP = new UnitProvider();
+
+
+int create_simulation()
+{
+    Log.Information("I will now be creating a new simulation, meaning a city will be created and populated");
+    return 0;
+}
 
 var x = new SimulatedUnit { Type = 1 };
 Log.Debug("Created a SU {@x}.", x);
