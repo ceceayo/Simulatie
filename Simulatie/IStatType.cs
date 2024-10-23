f53d0a909a910bf7cd36283782656b2bcc292a63
Johannes <133582@hondsrugcollege.nl>
2024-10-23 09:27:26 +0200

    Made IStatType and StatProvider.

---
+++ Simulatie/IStatType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulatie
+{
+    public interface IStatType
+    {
+        public int TypeNum { get; }
+        public int Id { get; }
+        public string Value { get; }
+        public void AskForValueInput(SimulationDatabaseContext db);
+    }
+}
---
+++ Simulatie/StatProvider.cs
@@ -0,0 +1,42 @@
+﻿using Serilog;
+using Simulatie.UnitTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulatie
+{
+    public class StatProvider
+    {
+        public Dictionary<int, Type> Types = new Dictionary<int, Type>
+        {
+            // empty (for now >w< )
+        };
+        public StatProvider()
+        {
+            Log.Debug("Created a statprovider");
+        }
+        public Type? GetType(int type)
+        {
+            if (Types.ContainsKey(type))
+            {
+                return Types[type];
+            }
+            return null;
+        }
+        public IStatType? GetInstance(int id, SimulationDatabaseContext db)
+        {
+            var o = db.Statistics.Find(id);
+            if (o == null) { return null; }
+            if (Types.ContainsKey(o.Type))
+            {
+                var t = Types[o.Type];
+                var i = Activator.CreateInstance(t, [id, db]) as IStatType;
+                return i;
+            }
+            return null;
+        }
+    }
+}
