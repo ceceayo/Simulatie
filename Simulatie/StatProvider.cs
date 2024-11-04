3af6d8e41e9daa286e2dcb552d8ee81abfacb955
jesse <133413@hondsrugcollege.nl>
2024-11-04 14:40:24 +0100

    add StatProvider.FindInstance for getting stats.

--- Simulatie/StatProvider.cs
+++ Simulatie/StatProvider.cs
@@ -39,5 +39,32 @@ namespace Simulatie
             }
             return null;
         }
+        public IStatType FindInstance(SimulationDatabaseContext db, int role, int type, Simulation sim)
+        {
+            var o = db.Statistics.Where(x => x.Role == role && x.Type == type && x.Owner == sim).ToList();
+            if (o.Count == 0)
+            {
+                var i = this.GetType(type);
+                if (i != null)
+                {
+                    var x = Activator.CreateInstance(i, new object[] { -1, db }) as IStatType;
+                    Log.Debug("Created DB item for stat with {role} of {type}: {x}", role, type, x);
+                    Log.Information("Please, enter a value for stat with role {role} of type {type}.", role, type);
+                    x.AskForValueInput(db);
+                    return x;
+                }
+            }
+            else if (o.Count == 1)
+            {
+                return this.GetInstance(o[0].Id, db);
+            }
+            else
+            {
+                Log.Fatal("Found multiple stats with role {role} of type {type}.", role, type);
+                throw new Exception("Found multiple stats with role and type");
+            }
+            Log.Fatal("Could not find or create stat")
+            throw new Exception("Could not find or create stat");
+        }
     }
 }
