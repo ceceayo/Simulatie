f8f87590576618bf83f02484f338329610793744
Mees <133041@hondsrugcollege.nl>
2024-11-20 10:05:26 +0100

    Added phonecharger

--- Simulatie/UnitProvider.cs
+++ Simulatie/UnitProvider.cs
@@ -17,6 +17,7 @@ namespace Simulatie
             { 7, typeof(Schoollamp) },
             { 8, typeof(Schoolcafetaria) },
             { 9, typeof(Classroom) },
+            { 10, typeof(Phonecharger) },
         };
 
         public UnitProvider()
