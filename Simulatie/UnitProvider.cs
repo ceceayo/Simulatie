5ab91552eb18ba0d629b18d2779561405bc9a83a
Johannes <133582@hondsrugcollege.nl>
2024-12-02 14:58:37 +0100

    Added dryer

--- Simulatie/UnitProvider.cs
+++ Simulatie/UnitProvider.cs
@@ -23,6 +23,7 @@ namespace Simulatie
             { 13, typeof(PersonalComputer) },
             { 14, typeof(HospitalLamp) },
             { 15, typeof(Airconditioningunit) },
+            { 17, typeof(Dryer) },
         };
 
         public UnitProvider()
---
+++ Simulatie/UnitTypes/Dryer.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+using Simulatie.StatTypes;
+
+namespace Simulatie.UnitTypes
+{
+    public class Dryer : IUnitType
+    {
+        public int TypeNum { get; } = 17;
+        public int Id { get; set; }
+        public Dictionary<int, string> Arguments { get; set; } = new Dictionary<int, string>();
+        public IUnitType Owner { get; set; }
+        public Dryer(int id, Dictionary<int, string> args, IUnitType owner)
+        {
+            this.Id = id;
+            this.Arguments = args;
+            Owner = owner;
+            Log.Debug("Create IUnitType instance of type Refrigerator with value {val}.", this);
+        }
+
+        public UnitTickResponse? OnTick(SimulationDatabaseContext db, StatProvider sp, UnitProvider up, Simulation sim)
+        {
+            SimpleNumber statInstance = sp.FindInstance(db, 17*10000+1, 1, sim, "Electricity used by dryer(s)") as SimpleNumber;
+            return new UnitTickResponse
+            {
+                NewUnit = new Refrigerator(args: this.Arguments, id: this.Id, owner: Owner),
+                ResourcesUsed = statInstance != null ? statInstance.GetNumber() : 700 // Assuming IStatType has an Id property
+            };
+        }
+        public List<IUnitType> OnCreate(SimulationDatabaseContext db, StatProvider sp, UnitProvider up, Simulation sim)
+        {
+            return new List<IUnitType> { };
+        }
+    }
+}
